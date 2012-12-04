3eaec758bd8cac1fa7fe1a8eef808380d44072e5
Matt Emborsky <memborsky@gmail.com>
2012-12-04 16:12:02 -0500

    Base project setup. This includes a simple web landing page on /base/templates/base/index.html for <host>/.

---
+++ sandbox/urls.py
@@ -0,0 +1,25 @@
+# The url routing methods we always need to include.
+from django.conf.urls import patterns, include, url
+
+# This allows us to send page request direct to template so we don't have to write
+# the same type of view over and over again.
+from django.views.generic.simple import direct_to_template
+
+# Uncomment the next two lines to enable the admin:
+#from django.contrib import admin
+#admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'sandbox.views.home', name='home'),
+    # url(r'^sandbox/', include('sandbox.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    #url(r'^admin/', include(admin.site.urls)),
+
+    # (web) / -> (path) /base/templates/index.html
+    url(r'^$', direct_to_template, {'template': 'base/index.html'}),
+)
